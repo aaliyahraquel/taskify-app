98eb6dfc704472ec26da763bad071d3f45ccc16a
aaliyahraquel <liyah_r_j@live.co.uk>
2024-12-16 14:25:56 +0000

    add todo card functionality with styling

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FaEdit } from "react-icons/fa";
 import { MdDelete, MdDone } from "react-icons/md";
 import { Todo } from './model';
@@ -12,18 +12,55 @@ type Props = {
 
 
 const TodoCard = ({ todo, todos, setTodos }: Props) => {
+
+    const [edit, setEdit] = useState<boolean>(false)    
+    const [editTodo, setEditTodo] = useState<string>(todo.todo)
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map( (todo) => todo.id === id? { ...todo, isDone: !todo.isDone } : todo))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
+
+    const handleEdit = (e:React.FormEvent, id:number) => {
+        e.preventDefault()
+        setTodos(todos.map((todo) => todo.id === id? {...todo, todo: editTodo}: todo))
+        setEdit(false)
+    }
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        inputRef.current?.focus()
+    }, [edit])
+
   return (
-    <form className="todos__single">
-        <span className="todos__single--text">{todo.todo}</span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+
+        {edit ? (
+            <input 
+            ref={inputRef}            
+            value={editTodo} 
+            onChange={(e) => setEditTodo(e.target.value)} 
+            className="todos__single--text"/>
+        ): todo.isDone ? (
+            <s className="todos__single--text">{todo.todo}</s>
+        ): (
+            <span className="todos__single--text">{todo.todo}</span>
+        )}
+
         <div>
-            <span className="icons">
+            <span className="icons" onClick={()=> !edit && !todo.isDone? setEdit(!edit): edit}
+            >
                 <FaEdit />
             </span>
             <span className="icons">
-                <MdDelete />
+                <MdDelete onClick={()=> handleDelete(todo.id)}/>
             </span>
             <span className="icons">
-                <MdDone />
+                <MdDone onClick={()=> handleDone(todo.id)}/>
             </span>
         </div>
     </form>
